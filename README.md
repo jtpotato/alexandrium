c3dbf644843bd858958cfba54adbaa307740283b
Joel <58995538+jtpotato@users.noreply.github.com>
2023-01-02 14:35:59 +1100

    slightly adjust styling

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Alexandrium
 Taken from the name Alexandria - as in the Library of Alexandria, and the suffix "-ium", used to denote chemical elements.
+
+A TikTok-esque media platform designed solely around the sharing of educational content.
+
+## Upcoming Features
+- Attach a Wikipedia article to a video??
--- src/lib/ui/BottomBar.svelte
+++ src/lib/ui/BottomBar.svelte
@@ -1,8 +1,8 @@
-<div class="absolute bottom-0 bg-stone-900 h-12 w-screen">
-	<div class="w-full h-full flex justify-center items-center">
+<div class="absolute bottom-0 bg-stone-900 w-screen">
+	<div class="w-full h-full flex justify-center items-center p-2">
 		<!--Our cool circle-->
 		<div
-			class="rounded-full bg-gradient-to-tr from-cyan-400 to-purple-500 p-1 flex justify-center items-center"
+			class="rounded-full bg-gradient-to-tr from-cyan-400 to-purple-500 p-1 flex justify-center items-center cursor-pointer"
 		>
 			<div class="bg-stone-800 h-10 w-10 rounded-full flex justify-center items-center backdrop-filter backdrop-blur-sm bg-opacity-30">
 				<span class="material-symbols-outlined text-white select-none">publish</span>
